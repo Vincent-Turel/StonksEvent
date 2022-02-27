c151f098e366d0dbf56c7f4b5864f52463a115f9
Dan Nakache <dan18.nakache@gmail.com>
2022-02-27 14:22:29 +0100

    InMemoryDatabase corrected

--- backend/src/main/java/fr/stonksdev/backend/components/InMemoryDatabase.java
+++ backend/src/main/java/fr/stonksdev/backend/components/InMemoryDatabase.java
@@ -1,6 +1,7 @@
 package fr.stonksdev.backend.components;
 
 import fr.stonksdev.backend.entities.Activity;
+import fr.stonksdev.backend.entities.StonksEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.List;
 @Component
 public class InMemoryDatabase {
     private int eventCounter = 0;
-    private List<Activity> eventList;
+    private List<StonksEvent> eventList;
 
     public void incrementEvents() { eventCounter++; }
     public int howManyEvents() { return eventCounter; }
